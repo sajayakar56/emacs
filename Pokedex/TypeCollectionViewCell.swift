7692c78520bdfeb0538ff253edf28e9f29c23987
Stephen Jayakar <ajay@Stephens-MacBook-Pro.local>
2017-09-21 01:03:02 -0700

    some permission changes, added functionality with collectionview selection

--- Pokedex/TypeCollectionViewCell.swift
+++ Pokedex/TypeCollectionViewCell.swift
@@ -14,6 +14,17 @@ class TypeCollectionViewCell: UICollectionViewCell {
         typeImageView = UIImageView(frame: contentView.frame)
         typeImageView.contentMode = .scaleAspectFill
         typeImageView.clipsToBounds = true
+        typeImageView.backgroundColor = UIColor(white: 1, alpha: 0)
+        typeImageView.layer.cornerRadius = 8
         contentView.addSubview(typeImageView)
+    }
+    
+    // consider adding a lerp function
+    func selectViewCell() {
+        typeImageView.transform = CGAffineTransform(scaleX: 0.75, y: 0.75)
+    }
+    
+    func deselectViewCell() {
+        typeImageView.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
     }
 }
